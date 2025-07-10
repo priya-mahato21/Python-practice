4b0dfe38503cadbee077a489ff03adcf7dd62dce
Priya Mahato <priyamahato2007@gmail.com>
2025-07-10 10:13:58 +0000

    Added student grade calculator

---
+++ student-grade-calculator.py
@@ -0,0 +1,16 @@
+marks = float(input("Enter your marks (out of 100): "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F (Fail)"
+
+print(f"Your grade is: {grade}")
